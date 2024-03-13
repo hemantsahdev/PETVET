ada275da2aa9631d1d51dd0d3971ca562b059b38
Hemant <103585067+hemantsahdev@users.noreply.github.com>
2024-03-13 10:50:35 +0530

    Update user.js

--- backend/src/api/Routes/users/public/user.js
+++ backend/src/api/Routes/users/public/user.js
@@ -45,3 +45,85 @@ router.post("/signup",async(req,res)=>{
 
 
 module.exports=router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express=require('express');
+const router=express.Router();
+
+
+const USERS=require("../../Model/user");
+
+const jwt=require("jsonwebtoken")
+
+const dotenv=require("dotenv")
+dotenv.config();
+
+const bcrypt=require("bcrypt")
+
+
+const {validateUser}=require("../../validate");
+
+router.post("/signup",async(req,res)=>{
+
+    const {username,password}=req.body;
+
+    // validation
+    const {error}=validateUser(req.body);
+    if(error) return res.status(400).send(error.details[0].message)
+
+    // already Exists
+    const usernameExists=await USERS.findOne({username})
+    if(usernameExists) res.status(400).json({message:"Username Already Exists"})
+   
+    // hash passwords
+    const hashedPass=await bcrypt.hash(password,10);
+
+
+    // create
+    const user=new USERS({
+        username,
+        password:hashedPass,
+        purchasedCourses:[]
+    })
+
+    try{
+        const userSaved = await user.save();
+        const payload={
+            username
+        }
+        const userToken = jwt.sign(payload, process.env.TOKEN_SECRET_USER)
+       
+        res.json({
+            id:userSaved._id,
+            message:"User Created Successfully!",
+            token:userToken
+        })
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send("User not Created!")
+    }
+
+
+})
